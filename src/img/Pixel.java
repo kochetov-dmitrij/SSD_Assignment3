bee81dce44e5113e66193685535bc86797f93ac3
S7rizh <antonov9312@yandex.ru>
2020-02-25 13:06:58 +0300

    Pixel rgba fix + removed ImgViewer

--- src/img/Pixel.java
+++ src/img/Pixel.java
@@ -1,10 +1,11 @@
 package img;
 
 public class Pixel {
-    public char r, g, b;
-    public Pixel(char r, char g, char b) {
+    public char r, g, b, a;
+    public Pixel(char r, char g, char b, char a) {
         this.r = r;
         this.g = g;
         this.b = b;
+        this.a = a;
     }
 }
